9a708b39dd670721f19c44055fe2afafcbe2f43f
Juan Pablo Biondo <jupbiondo@gmail.com{>
2024-10-14 22:56:35 -0300

    Add popup and localstorage functionality

--- main.js
+++ main.js
@@ -2,3 +2,48 @@ import { renderCategories } from "./src/services/categories";
 import './style.css';
 
 renderCategories();
+
+
+// ==== PRODUCT ====
+const btnAdd = document.querySelector(".buttonAdd");
+
+btnAdd.addEventListener("click", () => {
+    openModal();
+});
+
+
+// ==== POPUP ====
+const btnCancel = document.querySelector("#cancelBtn");
+
+btnCancel.addEventListener("click", () => {
+    closeModal();
+})
+
+
+// open close modal functions
+const openModal = () => {
+    const modal = document.querySelector("#modalPopUp");
+    modal.style.display = 'flex';
+}
+
+const closeModal = () => {
+    const modal = document.querySelector("#modalPopUp");
+    modal.style.display = 'none';
+}
+
+//save or modify elements
+
+const btnAccept = document.querySelector("#acceptBtn");
+
+btnAccept.addEventListener("click", () => {
+    handleSaveOrModifyElements();
+})
+
+const handleSaveOrModifyElements = () => {
+    const nombre = document.querySelector("#nombre").value;
+    const img = document.querySelector("#img").value;
+    const precio = document.querySelector("#precio").value;
+    const categoria = document.querySelector("#categoria").value;
+
+    closeModal();
+}
